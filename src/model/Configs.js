5485fb29320e5449258beecc612c129296004baa
John DeLeo <jdeleo@fastmail.com>
2023-10-10 10:27:13 -0400

    Changed purple color back to green.

--- src/model/Configs.js
+++ src/model/Configs.js
@@ -3,22 +3,22 @@ export const config_4x4 = {
     "numRows" : "4",
     "numColumns" : "4",
     "baseSquares" : [
-        { "color" : "purple", "row": "0", "column" : "0" },
+        { "color" : "green", "row": "0", "column" : "0" },
         { "color" : "yellow", "row": "0", "column" : "1" },
         { "color" : "grey", "row": "0", "column" : "2" },
         { "color" : "blue", "row": "0", "column" : "3" },
 
-        { "color" : "purple", "row": "1", "column" : "0" },
+        { "color" : "green", "row": "1", "column" : "0" },
         { "color" : "grey", "row": "1", "column" : "1" },
         { "color" : "yellow", "row": "1", "column" : "2" },
         { "color" : "blue", "row": "1", "column" : "3" },
 
-        { "color" : "purple", "row": "2", "column" : "0" },
+        { "color" : "green", "row": "2", "column" : "0" },
         { "color" : "yellow", "row": "2", "column" : "1" },
         { "color" : "grey", "row": "2", "column" : "2" },
         { "color" : "blue", "row": "2", "column" : "3" },
 
-        { "color" : "purple", "row": "3", "column" : "0" },
+        { "color" : "green", "row": "3", "column" : "0" },
         { "color" : "grey", "row": "3", "column" : "1" },
         { "color" : "yellow", "row": "3", "column" : "2" },
         { "color" : "blue", "row": "3", "column" : "3" } 
@@ -33,22 +33,22 @@ export const config_5x5 = {
         { "color" : "orange", "row": "0", "column" : "0" },
         { "color" : "red", "row": "0", "column" : "1" },
         { "color" : "purple", "row": "0", "column" : "2" },
-        { "color" : "purple", "row": "0", "column" : "3" },
+        { "color" : "green", "row": "0", "column" : "3" },
         { "color" : "blue", "row": "0", "column" : "4" },
 
         { "color" : "red", "row": "1", "column" : "0" },
         { "color" : "orange", "row": "1", "column" : "1" },
-        { "color" : "purple", "row": "1", "column" : "2" },
+        { "color" : "green", "row": "1", "column" : "2" },
         { "color" : "blue", "row": "1", "column" : "3" },
         { "color" : "yellow", "row": "1", "column" : "4" },
 
         { "color" : "purple", "row": "2", "column" : "0" },
-        { "color" : "purple", "row": "2", "column" : "1" },
+        { "color" : "green", "row": "2", "column" : "1" },
         { "color" : "orange", "row": "2", "column" : "2" },
         { "color" : "yellow", "row": "2", "column" : "3" },
         { "color" : "purple", "row": "2", "column" : "4" },
         
-        { "color" : "purple", "row": "3", "column" : "0" },
+        { "color" : "green", "row": "3", "column" : "0" },
         { "color" : "blue", "row": "3", "column" : "1" },
         { "color" : "yellow", "row": "3", "column" : "2" },
         { "color" : "orange", "row": "3", "column" : "3" },
@@ -70,7 +70,7 @@ export const config_6x6 = {
         { "color" : "red", "row": "0", "column" : "0" },
         { "color" : "pink", "row": "0", "column" : "1" },
         { "color" : "purple", "row": "0", "column" : "2" },
-        { "color" : "purple", "row": "0", "column" : "3" },
+        { "color" : "green", "row": "0", "column" : "3" },
         { "color" : "yellow", "row": "0", "column" : "4" },
         { "color" : "red", "row": "0", "column" : "5" },
 
@@ -81,7 +81,7 @@ export const config_6x6 = {
         { "color" : "orange", "row": "1", "column" : "4" },
         { "color" : "blue", "row": "1", "column" : "5" },
 
-        { "color" : "purple", "row": "2", "column" : "0" },
+        { "color" : "green", "row": "2", "column" : "0" },
         { "color" : "brown", "row": "2", "column" : "1" },
         { "color" : "blue", "row": "2", "column" : "2" },
         { "color" : "purple", "row": "2", "column" : "3" },
@@ -93,7 +93,7 @@ export const config_6x6 = {
         { "color" : "purple", "row": "3", "column" : "2" },
         { "color" : "blue", "row": "3", "column" : "3" },
         { "color" : "brown", "row": "3", "column" : "4" },
-        { "color" : "purple", "row": "3", "column" : "5" },
+        { "color" : "green", "row": "3", "column" : "5" },
 
         { "color" : "blue", "row": "4", "column" : "0" },
         { "color" : "orange", "row": "4", "column" : "1" },
@@ -104,7 +104,7 @@ export const config_6x6 = {
 
         { "color" : "red", "row": "5", "column" : "0" },
         { "color" : "yellow", "row": "5", "column" : "1" },
-        { "color" : "purple", "row": "5", "column" : "2" },
+        { "color" : "green", "row": "5", "column" : "2" },
         { "color" : "purple", "row": "5", "column" : "3" },
         { "color" : "pink", "row": "5", "column" : "4" },
         { "color" : "red", "row": "5", "column" : "5" },
